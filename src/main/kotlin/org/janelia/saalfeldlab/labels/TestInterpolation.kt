09c5dd521e55a893b8c6c7fc0e2bb5dbd9c0cdd9
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-08-03 16:11:53 -0400

    Fix TestInterpolation

--- src/main/kotlin/org/janelia/saalfeldlab/labels/TestInterpolation.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/labels/TestInterpolation.kt
@@ -2,18 +2,21 @@ package org.janelia.saalfeldlab.labels
 
 import gnu.trove.map.hash.TLongIntHashMap
 import net.imagej.ImageJ
+import net.imagej.display.ImageDisplay
 import net.imglib2.RandomAccessibleInterval
 import net.imglib2.algorithm.neighborhood.HyperSphereShape
 import net.imglib2.converter.Converter
 import net.imglib2.converter.Converters
-import net.imglib2.converter.readwrite.ARGBChannelSamplerConverter
 import net.imglib2.img.array.ArrayImgFactory
 import net.imglib2.img.array.ArrayImgs
 import net.imglib2.type.numeric.ARGBType
 import net.imglib2.type.numeric.IntegerType
-import net.imglib2.type.numeric.integer.UnsignedLongType
+import net.imglib2.type.numeric.integer.LongType
+import net.imglib2.type.numeric.real.DoubleType
 import net.imglib2.view.Views
 import java.util.*
+import java.util.stream.Collectors
+import java.util.stream.Stream
 import kotlin.math.roundToLong
 
 private fun <I : IntegerType<I>> withRandomColors(
@@ -33,13 +36,16 @@ private fun <I : IntegerType<I>> withRandomColors(
 
 fun main(args: Array<String>) {
 	val dim = longArrayOf(200, 100)
-	println("${dim.asList()}")
 	val img1 = ArrayImgs.unsignedLongs(*dim)
 	val img2 = ArrayImgs.unsignedLongs(*dim)
-	val interpolated1 = ArrayImgs.unsignedLongs(*dim)
-	val interpolated2 = ArrayImgs.unsignedLongs(*dim)
-	interpolated1.forEach({ it.set(-1) })
-	interpolated2.forEach({ it.set(-1) })
+	val numSteps = 100
+	val interpolations = Stream
+			.generate { ArrayImgs.unsignedLongs(*dim) }
+			.limit(numSteps.toLong())
+			.collect(Collectors.toList())
+			.toTypedArray()
+	interpolations.forEach { it.forEach { it.set(-1) } }
+
 
 	val rng = Random(100)
 	val radii = longArrayOf(10, 5, 100, 25)
@@ -52,17 +58,17 @@ fun main(args: Array<String>) {
 
 	for (radiiAndPositions in longArrayOf(1, 2, 3, 4) zip (radii zip positions)) {
 		val r1 = radiiAndPositions.second.first
-		val r2 = (r1 * (1 + rng.nextGaussian() * 0.4)).roundToLong()
+		val r2 = (r1 * (1 + rng.nextGaussian() * 0.2)).roundToLong()
 		val p = radiiAndPositions.second.second
 
 		val sphere1 = HyperSphereShape(r1)
 		val sphere2 = HyperSphereShape(r2)
 
 		val access1 = sphere1.neighborhoodsRandomAccessible(Views.extendZero(img1)).randomAccess()
-		val access2 = sphere1.neighborhoodsRandomAccessible(Views.extendZero(img2)).randomAccess()
+		val access2 = sphere2.neighborhoodsRandomAccessible(Views.extendZero(img2)).randomAccess()
 
 		access1.setPosition(p)
-		access2.setPosition(longArrayOf(p[0] + rng.nextInt(10) - 2, p[1] + rng.nextInt(10) - 2))
+		access2.setPosition(longArrayOf(p[0] + rng.nextInt(11) - 5, p[1] + rng.nextInt(11) - 5))
 
 		val id = radiiAndPositions.first
 		access1.get().forEach({ it.set(id) })
@@ -72,22 +78,23 @@ fun main(args: Array<String>) {
 	InterpolateBetweenSections.interpolateBetweenSections(
 			img1,
 			img2,
-			ArrayImgFactory(UnsignedLongType()),
-			interpolated1,
-			interpolated2
+			ArrayImgFactory(DoubleType()),
+			*interpolations
 	)
 
 	val ij = ImageJ()
 
-	val stacked = Views.stack(img1, interpolated1, interpolated2, img2)
-	val colored = withRandomColors(stacked, rng = rng)
-	val withChannels = Views.stack(
-			Converters.convert(colored, ARGBChannelSamplerConverter(0)),
-			Converters.convert(colored, ARGBChannelSamplerConverter(1)),
-			Converters.convert(colored, ARGBChannelSamplerConverter(2))
-	)
+	val stacked = Views.stack(*(listOf(img1) + interpolations + listOf(img2)).toTypedArray())
+//	val colored = withRandomColors(stacked, rng = rng)
+//	val withChannels = Views.stack(
+//			Converters.convert(colored, ARGBChannelSamplerConverter(0)),
+//			Converters.convert(colored, ARGBChannelSamplerConverter(1)),
+//			Converters.convert(colored, ARGBChannelSamplerConverter(2))
+//	)
 	ij.ui().showUI()
-	ij.ui().show("colored", withChannels)
-
+	val display = ij.display().createDisplay("stacked", Converters.convert(stacked, { s,t -> t.set(s.get()) }, LongType()))
+	val lutName = "NCSA PalEdit/16_colors.lut"
+	val lut = ij.lut().loadLUT(ij.lut().findLUTs().get(lutName)!!)!!
+	ij.lut().applyLUT(lut, display as ImageDisplay)
 
 }
