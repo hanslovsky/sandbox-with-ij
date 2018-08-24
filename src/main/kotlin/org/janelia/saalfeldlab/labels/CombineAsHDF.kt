39b089b1b7988f84dc6f2ff1b766da37bbb1c59a
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-08-24 16:41:38 -0400

    Adjust offsets

--- src/main/kotlin/org/janelia/saalfeldlab/labels/CombineAsHDF.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/labels/CombineAsHDF.kt
@@ -15,6 +15,9 @@ import org.janelia.saalfeldlab.n5.N5FSReader
 import org.janelia.saalfeldlab.n5.hdf5.N5HDF5Reader
 import org.janelia.saalfeldlab.n5.hdf5.N5HDF5Writer
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.lang.invoke.MethodHandles
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -25,8 +28,11 @@ import java.util.stream.StreamSupport
 
 private val USER_HOME = System.getProperty("user.home")
 
-public class CombineAsHDF {
+class CombineAsHDF {
 	companion object {
+
+		private val LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
 		fun run(args: Array<String>) {
 			val groundTruthContainer = "$USER_HOME/local/tmp/sample_A_padded_20160501-interpolated-labels-2-additional-sections.n5"
 			val groundTruthDataset = "volumes/labels/neuron_ids"
@@ -41,14 +47,24 @@ public class CombineAsHDF {
 			val rawAttributes = rawN5.getDatasetAttributes(rawDataset)
 
 			val scaleFactor = 3
-			val voxelSizeFactor = scaleFactor.toDouble()
-			val groundTruthResolution = DoubleStream.of(40.0 / voxelSizeFactor, 4.0, 4.0).map { it * voxelSizeFactor }.toArray()
+			val voxelSizeFactor = scaleFactor.toDouble() * 3.0
+			val groundTruthResolution = DoubleStream.of(40.0 / scaleFactor.toDouble(), 4.0, 4.0).map { it * voxelSizeFactor }.toArray()
 			val groundTruthOffset = DoubleStream.of(1520.0, 3644.0, 3644.0).map { it * voxelSizeFactor }.toArray()
+			groundTruthOffset[0] = groundTruthOffset[0] - groundTruthResolution[0] / scaleFactor.toDouble()
 
 			val rawResolution = DoubleStream.of(40.0, 4.0, 4.0).map { it * voxelSizeFactor }.toArray()
 			val rawOffset = doubleArrayOf(0.0, 0.0, 0.0)
 
-			val targetContainer = "/groups/saalfeld/home/hanslovskyp/sample_A_padded_20160501-2-additional-sections.h5"
+			LOG.info("raw resolution: {}", rawResolution)
+			LOG.info("raw offset:     {}", rawOffset)
+
+			LOG.info("gt resolution:  {}", groundTruthResolution)
+			LOG.info("gt offset:      {}", groundTruthOffset)
+
+			val outputDir = "/groups/saalfeld/home/hanslovskyp/data/cremi/training"
+			File(outputDir).mkdirs()
+			val targetContainer = "$outputDir/sample_A_padded_20160501-2-additional-sections.h5"
+			LOG.info("target container: ${targetContainer}")
 			val targetN5 = N5HDF5Writer(targetContainer, 64, 64, 64)
 			targetN5.createDataset(rawDataset, rawAttributes)
 			targetN5.createDataset(groundTruthDataset, groundTruthAttributes)
@@ -80,7 +96,7 @@ public class CombineAsHDF {
 					.collect(Collectors.toList())
 			val downsampled = Views.stack(downsampledSections)
 			sw.stop()
-			println("Finished downsampling in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
+			LOG.info("Finished downsampling in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
 
 			val downsampledAttributes = DatasetAttributes(
 					Intervals.dimensionsAsLongArray(downsampled),
@@ -92,27 +108,33 @@ public class CombineAsHDF {
 			val downsampledResolution = groundTruthResolution.clone()
 			downsampledResolution[1] = downsampledResolution[1] * scaleFactor
 			downsampledResolution[2] = downsampledResolution[2] * scaleFactor
+			val downSampledOffset = groundTruthOffset.clone()
+			// TODO how to set correct offset? Is this the right thing to do?
+			downSampledOffset[1] = downSampledOffset[1] - downsampledResolution[1] / scaleFactor.toDouble()
+			downSampledOffset[2] = downSampledOffset[2] - downsampledResolution[2] / scaleFactor.toDouble()
 
 			sw.start()
 			N5Utils.save(downsampled, targetN5, downsampledDataset, downsampledAttributes.blockSize, downsampledAttributes.compression, es)
 			sw.stop()
-			println("Finished saving downsampled data in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
+			LOG.info("Finished saving downsampled data in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
 
 			sw.start()
 			N5Utils.save(raw, targetN5, rawDataset, rawAttributes.blockSize, rawAttributes.compression, es)
 			sw.stop()
-			println("Finished copying raw data in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
+			LOG.info("Finished copying raw data in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
 			sw.start()
 			N5Utils.save(groundTruth, targetN5, groundTruthDataset, groundTruthAttributes.blockSize, groundTruthAttributes.compression, es)
 			sw.stop()
-			println("Finished copying label data in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
+			LOG.info("Finished copying label data in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
 			es.shutdown()
 
 			sw.start()
-			val maxId = StreamSupport.stream(Views.flatIterable(groundTruth).spliterator(), false).mapToLong(UnsignedLongType::getIntegerLong).reduce { l1, l2 -> Math.max(l1, l2) }
+			val maxId = StreamSupport
+					.stream(Views.flatIterable(groundTruth).spliterator(), false)
+					.mapToLong(UnsignedLongType::getIntegerLong).reduce { l1, l2 -> Math.max(l1, l2) }
 			sw.stop()
 
-			println("maxId=$maxId in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
+			LOG.info("maxId=$maxId in ${TimeUnit.NANOSECONDS.toSeconds(sw.nanoTime())}s")
 
 			targetN5.setAttribute(rawDataset, "resolution", rawResolution)
 			targetN5.setAttribute(rawDataset, "offset", rawOffset)
@@ -120,7 +142,7 @@ public class CombineAsHDF {
 			targetN5.setAttribute(groundTruthDataset, "offset", groundTruthOffset)
 			targetN5.setAttribute(groundTruthDataset, "maxId", maxId.asLong)
 			targetN5.setAttribute(downsampledDataset, "resolution", downsampledResolution)
-			targetN5.setAttribute(downsampledDataset, "offset", groundTruthOffset)
+			targetN5.setAttribute(downsampledDataset, "offset", downSampledOffset)
 			targetN5.setAttribute(downsampledDataset, "maxId", maxId.asLong)
 
 		}
