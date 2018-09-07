22581163b47e0c0035825e232199519d0a4c5e5c
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-09-07 07:58:55 -0400

    Remove unnecessary clutter

--- src/main/kotlin/net/imglib2/view/MixedTransformComponentMappingTest.kt
+++ src/main/kotlin/net/imglib2/view/MixedTransformComponentMappingTest.kt
@@ -35,7 +35,7 @@ fun main(args: Array<String>) {
 	val dims = longArrayOf(1, 2, 3)
 	val rng = Random(100L)
 	val img = ArrayImgs.longs(*dims)
-	img.forEach { it.integer = rng.nextInt(1024) - 512 }
+	img.forEach { it.integer = rng.nextInt(1024) }
 
 	val permutation = intArrayOf(2, 0, 1)
 	val permuted = MixedTransformComponentMappingTest.permute(img, permutation)
@@ -44,30 +44,6 @@ fun main(args: Array<String>) {
 	log.info("permutation:      {}", permutation)
 	log.info("img min max:      {} {}", Intervals.minAsLongArray(img), Intervals.maxAsLongArray(img))
 	log.info("permuted min max: {} {}", Intervals.minAsLongArray(permuted), Intervals.maxAsLongArray(permuted))
-
-	val imgAccess = img.randomAccess()
-	val permutedCursor = Views.iterable(permuted).cursor()
-
-	while(permutedCursor.hasNext())
-	{
-		val actual = permutedCursor.next()
-		imgAccess.setPosition(permutedCursor.getLongPosition(0), permutation[0])
-		imgAccess.setPosition(permutedCursor.getLongPosition(1), permutation[1])
-		imgAccess.setPosition(permutedCursor.getLongPosition(2), permutation[2])
-
-		imgAccess.setPosition(permutedCursor.getLongPosition(permutation[0]), 0)
-		imgAccess.setPosition(permutedCursor.getLongPosition(permutation[1]), 1)
-		imgAccess.setPosition(permutedCursor.getLongPosition(permutation[2]), 2)
-
-		val expected = imgAccess.get()
-		log.info("expected={} actual={}", expected, actual)
-	}
-
-	var actualSum = 0L
-	Views.flatIterable(permuted).forEach { log.trace("permuted value={}", it); actualSum += it.integerLong }
-	var expectedSum = 0L
-	img.forEach { log.trace("original value={}", it); expectedSum += it.integerLong }
-	log.info("sum expected={} actual={}", expectedSum, actualSum)
 
 	val expectedValues = TLongHashSet(img.update(null).currentStorageArray)
 	val actualValues = TLongHashSet()
